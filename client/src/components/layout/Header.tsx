163bc1a12bf5b7fe06ffad04d88e98ecaae65805
Gwon Hong <gwonhong.dev@gmail.com>
2025-03-12 14:47:51 +0900

    :sparkles: Feat: 현재 멤버 컨텍스트 헤더와 메인 페이지에 적용

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,18 +1,22 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import CurrentMemberContext from "@/contexts/CurrentMemberContext";
+import { use } from "react";
 import { BsPersonCircle } from "react-icons/bs";
 
-const Header = () => {
+function Header() {
+  const { currentMember } = use(CurrentMemberContext);
+
   return (
     <header className="flex justify-between items-center p-4">
       <h1 className="text-xl font-bold">AI Vision inside</h1>
       <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="User avatar" />
+        <AvatarImage src={currentMember?.avatar} alt="User avatar" />
         <AvatarFallback>
           <BsPersonCircle className="w-5 h-5" />
         </AvatarFallback>
       </Avatar>
     </header>
   );
-};
+}
 
 export default Header;
