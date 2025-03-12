26300b569e897681d5a5d2933ad66dfa92fed587
Gwon Hong <gwonhong.dev@gmail.com>
2025-03-12 16:39:50 +0900

    :sparkles: Feat: 유저 변경 드랍다운 메뉴 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import CurrentMemberContext from "@/contexts/CurrentMemberContext";
 import Member from "@/interfaces/Member";
 import MainPage from "@/pages/MainPage";
 import { useMemo, useState } from "react";
+
 function App() {
   // TODO: 추후 로컬 서버에서 가져오는 데이터로 변경
   const initialMember: Member = {
     id: "1",
     name: "Gwon Hong",
-    avatar: "https://github.com/shadcn.png",
+    avatar: "https://avatars.githubusercontent.com/gwonhong",
   };
 
   // TODO: 로컬 서버와 websocket 통신을 통해 지속적으로 현재 멤버 상태를 업데이트 하는 로직 추가
