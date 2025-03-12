4f1abf98b4b3a5a1a2da6cb311057584dff44dd6
Gwon Hong <gwonhong.dev@gmail.com>
2025-03-12 14:47:26 +0900

    :sparkles: Feat: 현재 멤버 컨텍스트 위치 이동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
 import Header from "@/components/layout/Header";
+import CurrentMemberContext from "@/contexts/CurrentMemberContext";
+import Member from "@/interfaces/Member";
 import MainPage from "@/pages/MainPage";
-import CurrentMemberProvider from "@/providers/CurrentMemberProvider";
-
+import { useMemo, useState } from "react";
 function App() {
+  // TODO: 추후에 서버에서 가져오는 데이터로 변경
+  const initialMember: Member = {
+    id: "1",
+    name: "Gwon Hong",
+    avatar: "https://github.com/shadcn.png",
+  };
+
+  const [currentMember, setCurrentMember] = useState<Member | null>(initialMember);
+  const value = useMemo(() => ({ currentMember, setCurrentMember }), [currentMember]);
+
   return (
     <div className="max-w-md mx-auto bg-white min-h-screen">
-      <CurrentMemberProvider>
+      <CurrentMemberContext value={value}>
         <Header />
         <MainPage />
-      </CurrentMemberProvider>
+      </CurrentMemberContext>
     </div>
   );
 }
